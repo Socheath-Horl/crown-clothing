91d6c7ac407c0f172b9aa9356314a42cee3ff5b9
Socheath-Horl <horl.socheath@gmail.com>
2022-08-29 21:05:54 +0700

    fixed deploy warning

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -30,7 +30,7 @@ const firebaseConfig = {
   measurementId: "G-PJLHL5TMZ4"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
