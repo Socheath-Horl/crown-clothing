7a489289365cb02fbec16195e4207f7ac3cc9689
Socheath-Horl <horl.socheath@gmail.com>
2022-09-06 21:30:39 +0700

    redux-saga

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,16 +1,10 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
-
-import {
-  signInAuthUserWithEmailAndPassword,
-  signInWithGooglePopup,
-} from '../../utils/firebase/firebase.utils';
-import {
-  SignInContainer,
-  ButtonsContainer,
-} from './sign-in-form.styles';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
+import { googleSignInStart, emailSignInStart } from '../../store/user/user.action';
 
 const defaultFormFields = {
   email: '',
@@ -18,6 +12,7 @@ const defaultFormFields = {
 }
 
 const SignInForm = () => {
+  const dispatch = useDispatch();
   const [formFields, setFromFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
@@ -26,17 +21,14 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    await signInWithGooglePopup();
+    dispatch(googleSignInStart())
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      await signInAuthUserWithEmailAndPassword(
-        email,
-        password
-      );
+      dispatch(emailSignInStart(email, password));
       resetFormFields();
     } catch (error) {
       console.log('user sign in failed', error);
--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,23 @@ import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from 'redux-thunk';
+import createSagaMiddleware from "redux-saga";
+
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from "./root-reducer";
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
 const middleWares = [
   process.env.NODE_ENV === 'development' && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(
   Boolean
 );
@@ -19,12 +29,6 @@ const composeEnhancer =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
@@ -34,5 +38,7 @@ export const store = createStore(
   undefined, 
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
